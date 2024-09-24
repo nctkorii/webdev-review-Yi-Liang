e364cb2bc09dd6aff162abfcd028f6ca09a71b5b
nctkorii <135648547+nctkorii@users.noreply.github.com>
2024-09-24 12:10:56 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('changeTextButton').addEventListener('click', function() {
+    document.getElementById('dynamicText').innerHTML = 'The text has been changed!';
+});
+
+
+document.getElementById('button').addEventListener('click', function() {
+    document.getElementById('change').innerHTML = 'The text has been changed!';
+});
